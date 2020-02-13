7954263d2dc70fee524ff7f04436c6b0ec95db80
lishirui0306 <3171103@qq.com>
2020-02-13 19:17:19 +0800

    解决启动jackson报错版本问题

--- lsr-redis-lock/src/main/java/cn/lsr/redis/core/RedisLock.java
+++ lsr-redis-lock/src/main/java/cn/lsr/redis/core/RedisLock.java
@@ -1,10 +1,7 @@
 package cn.lsr.redis.core;
 
 import cn.lsr.redis.utils.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
