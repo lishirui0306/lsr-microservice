c26d55da680f6b4927c06784eff187a581b4b38b
lishirui0306 <3171103@qq.com>
2020-03-27 17:53:55 +0800

    新增批量sql脚本

--- lsr-batch-processing/src/main/java/cn/lsr/util/DateUtils.java
+++ lsr-batch-processing/src/main/java/cn/lsr/util/DateUtils.java
@@ -37,7 +37,9 @@ public class DateUtils {
      */
     private static Timestamp getDayStartTime(Date d) {
         Calendar calendar = Calendar.getInstance();
-        if (null != d) calendar.setTime(d);
+        if (null != d){
+            calendar.setTime(d);
+        }
         calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
         calendar.set(Calendar.MILLISECOND, 0);
         return new Timestamp(calendar.getTimeInMillis());
@@ -48,7 +50,9 @@ public class DateUtils {
      */
     private static Timestamp getDayEndTime(Date d) {
         Calendar calendar = Calendar.getInstance();
-        if (null != d) calendar.setTime(d);
+        if (null != d) {
+            calendar.setTime(d);
+        }
         calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 23, 59, 59);
         calendar.set(Calendar.MILLISECOND, 999);
         return new Timestamp(calendar.getTimeInMillis());
