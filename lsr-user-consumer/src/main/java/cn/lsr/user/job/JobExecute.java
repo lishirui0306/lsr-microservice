a5967bf5ebe594f2c1fbf1557fd97957db64e4f2
lishirui0306 <3171103@qq.com>
2020-05-05 15:34:18 +0800

    消费者模块单独抽离

--- lsr-user-consumer/src/main/java/cn/lsr/user/job/JobExecute.java
+++ lsr-user-consumer/src/main/java/cn/lsr/user/job/JobExecute.java
@@ -7,9 +7,6 @@ import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
-
 /**
  * @Description: job执行入口
  * @Package: lsr-microservice
